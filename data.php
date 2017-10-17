c69461ddba3581f73145ba8c6cf1c761db954d39
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2017-10-17 16:06:03 +0200

    adde a id array within my array.

--- data.php
+++ data.php
@@ -5,7 +5,8 @@ declare (strict_types=1);
 
 $posts = [
   ['author'=> 'Batman',
-  'Fullname' => 'Bruce Wayne',
+  'Fullname' => ['id'=>'001',
+  'name' => 'Bruce Wayne'],
   'content' =>'Hero can be anyone.
   Even a man knowing something as
   simple and reassuring as putting a coat around a young boy
@@ -15,20 +16,23 @@ $posts = [
 ],
 
   ['author'=> 'Albert',
-  'Fullname' => 'Albet Einstein',
+  'Fullname' => ['id'=> '002',
+  'name' => 'Albet Einstein'],
   'content' =>'Education is what
   remains after one has forgotten everything he learned in school',
   'date' => '2016-10-18'
 ],
 
   ['author'=> 'Leonardo',
-  'Fullname' => 'By: Leonardo DiCaprio',
+  'Fullname' => ['id' => '003',
+  'name' => 'By: Leonardo DiCaprio'],
   'content'=>'My mother is a walking miracle.',
   'date' =>'2010-07-16'
 ],
 
   ['author'=> 'Bane',
-  'Fullname' => 'By: Tom Hardy',
+  'Fullname' => ['ida' => '004',
+  'name' => 'By: Tom Hardy'],
   'content' =>'Ooh, you think darkness is your ally?
   You merely adopted the dark, I was born in it. Molded by it.
   I didn\'t see the light until I was already a man.
@@ -37,26 +41,30 @@ $posts = [
 ],
 
   ['author'=> 'Barney',
-  'Fullname' => 'By: Barney Stinson',
+  'Fullname' => ['id' => '005',
+  'name' => 'By: Barney Stinson'],
   'content' =>'It\'s going to be legen...wait for it...and I hope you\'re
   snot lactose-intolerant cause the second half of that word is...dairy!',
   'date' =>'2013-02-18'
 ],
 
   ['author'=> 'Batman',
-  'Fullname' => 'By: Bruce Wayne',
+  'Fullname' => ['id'=>'001',
+  'name' => 'Bruce Wayne'],
   'content' =>'No guns, no killing.',
   'date' =>'2012-07-20'
 ],
 
   ['author'=> 'Albert',
-  'Fullname' => 'By: Albet Einstein',
+  'Fullname' => ['id'=> '002',
+  'name' => 'Albet Einstein'],
   'content' =>'The only thing that interferes with my learning is my education.',
   'date' =>'2016-10-18'
 ],
 
   ['author'=> 'Leonardo',
-  'Fullname' => 'By: Leonardo DiCaprio',
+  'Fullname' => ['id' => '003',
+  'name' => 'By: Leonardo DiCaprio'],
   'content'=>'If I want to go to a party with a few male friends, it doesn\'t
   mean I\'m gay.',
   'date' =>'2010-07-16'
@@ -64,13 +72,15 @@ $posts = [
 
 
   ['author'=> 'Bane',
-  'Fullname' => 'By: Tom Hardy',
+  'Fullname' => ['ida' => '004',
+  'name' => 'By: Tom Hardy'],
   'content' =>'Do you feel in charge?',
   'date' =>'2012-07-20'
 ],
 
   ['author'=> 'Barney',
-  'Fullname' => 'By: Barney Stinson',
+  'Fullname' => ['id' => '005',
+   'name' => 'By: Barney Stinson'],
   'content' =>'When I\'m sad, I stop being sad, and be awesome instead! True Story..',
   'date' =>'2013-02-18'
 ],
