eeb5c228d64558e728a8a48bedb1861d7cb78242
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2017-10-17 11:30:58 +0200

    completed the array with all the data for my page

--- data.php
+++ data.php
@@ -4,24 +4,61 @@ declare (strict_types=1);
 
 
 $posts = [
-    ['author'=> 'Batman', 'Fullname' => 'Bruce Wayne', 'content' =>'Hero can be anyone.
-    Even a man knowing something as
-    simple and reassuring as putting a coat around a young boy shoulders to let him
-    know the world hadn\'t ended.','date' =>2012-07-20],
+  'Batman1'  =>['author'=> 'Batman',
+  'Fullname' => 'Bruce Wayne',
+  'content' =>'Hero can be anyone.
+  Even a man knowing something as
+  simple and reassuring as putting a coat around a young boy
+  shoulders to let him
+  know the world hadn\'t ended.','date' =>2012-07-20],
 
-    ['author'=> 'Hodor', 'Fullname' => 'Hodor Hodor', 'content' =>$text,'date' =>2016-10-18],
+  'Arbert1'  =>['author'=> 'Albert',
+  'Fullname' => 'Albet Einstein',
+  'content' =>'Education is what
+  remains after one has forgotten everything he learned in school',
+  'date' =>2016-10-18],
 
-    ['author'=> 'Leonardo', 'Fullname' => 'Leonardo DiCaprio','content'=>'My mother is a walking miracle.',
-    'date' =>2010-07-16],
+  'Leonardo1'  =>  ['author'=> 'Leonardo',
+  'Fullname' => 'Leonardo DiCaprio',
+  'content'=>'My mother is a walking miracle.',
+  'date' =>2010-07-16],
 
-    ['author'=> 'Bane', 'Fullname' => 'Tom Hardy','content' =>'Ooh, you think darkness is your ally?
-    You merely adopted the dark, I was born in it. Molded by it.
-    I didn\'t see the light until I was already a man. By then there was nothing to be but blinded.','date' =>2012-07-20],
+  'Bane1'  =>  ['author'=> 'Bane',
+  'Fullname' => 'Tom Hardy',
+  'content' =>'Ooh, you think darkness is your ally?
+  You merely adopted the dark, I was born in it. Molded by it.
+  I didn\'t see the light until I was already a man.
+  By then there was nothing to be but blinded.','date' =>2012-07-20],
 
-    ['author'=> 'Barney', 'Fullname' => 'Barney Stinson','content' =>'When pulling up to a stoplight,
-    a Bro lowers his window so that all might enjoy his music selection.
-    Corollary If there happens to be a hot chick driving the car next to the Bro, the Bro shall put his sunglasses
-    down to get a better look. If he\s not wearing his sunglasses, he will first put them on,
-    then pull down to get a better look','date' =>2013-02-18],
+  'Barney1'  =>  ['author'=> 'Barney',
+  'Fullname' => 'Barney Stinson',
+  'content' =>'It\'s going to be legen...wait for it...and I hope you\'re
+  not lactose-intolerant cause the second half of that word is...dairy!',
+  'date' =>2013-02-18],
+
+  'Batman2'  =>['author'=> 'Batman',
+  'Fullname' => 'Bruce Wayne',
+  'content' =>'No guns, no killing.','date' =>2012-07-20],
+
+  'Arbert2'  =>['author'=> 'Albert',
+  'Fullname' => 'Albet Einstein',
+  'content' =>'The only thing that interferes with my learning is my education.',
+  'date' =>2016-10-18],
+
+  'Leonardo2'  =>  ['author'=> 'Leonardo',
+  'Fullname' => 'Leonardo DiCaprio',
+  'content'=>'If I want to go to a party with a few male friends, it doesn\'t mean I\'m gay.
+Read more at.',
+  'date' =>2010-07-16],
+
+
+  'Bane2'  =>  ['author'=> 'Bane',
+  'Fullname' => 'Tom Hardy',
+  'content' =>'Do you feel in charge?','date' =>2012-07-20],
+
+  'Barney1'  =>  ['author'=> 'Barney',
+  'Fullname' => 'Barney Stinson',
+  'content' =>'When I\'m sad, I stop being sad, and be awesome instead! True Story..',
+  'date' =>2013-02-18],
 
 ];
